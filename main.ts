0ed5251b2208bbaa7e87e5663fa49ddb85626f9f
AlvaroPereda <peredalvaro18@gmail.com>
2024-12-20 11:17:47 -0500

    init graphql

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb"
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from "./typeDefs.ts";
+import { resolvers } from "./resolvers.ts";
 
 const MONGO_URL = Deno.env.get("MONGO_URL")
 if(!MONGO_URL) Deno.exit(1)
@@ -11,3 +13,11 @@ console.log("Conectado a la base de datos")
 
 const db = client.db("agenda")
 const ContactCollection = db.collection("contact")
+
+const server = new ApolloServer({typeDefs, resolvers})
+
+const { url } = await startStandaloneServer(server,{
+  context: async() => ({ ContactCollection })
+})
+
+console.log(`🚀  Server ready at: ${url}`);
